c027e35d6cf71023c82794d30998eb2032f86942
Jagadeesh YM <jagadeesh.ym@CF-LP00222.local>
2019-01-16 18:00:59 +0530

    removal of cloneModal (unneccessary obj)

--- src/CruxComponent.tsx
+++ src/CruxComponent.tsx
@@ -138,7 +138,6 @@ export class CruxComponentCreator {
                     showCreateModal: false,
                     showFilterModal: false,
                     model: {},
-                    cloneModel: {},
                     showCustomModal: false,
                     filterModel: {
                         paginate: {
