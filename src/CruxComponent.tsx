8255d35d19c4b75732fb976b2b69fc4d94e92fd3
Jagadeesh Y M <jagadeesh.ym@v.curefit.com>
2019-01-25 12:28:22 +0530

    align issues

--- src/CruxComponent.tsx
+++ src/CruxComponent.tsx
@@ -96,7 +96,8 @@ export class CruxComponentCreator {
             }
 
             checkAdditionalModel(modelName: string, props: any) {
-                if ((modelName === constants.modelName && (constants.paginate || (!_.isEqual(this.props.queryParams, props.queryParams))) ||
+                if ((modelName === constants.modelName 
+                        && (constants.paginate || (!_.isEqual(this.props.queryParams, props.queryParams))) ||
                     !Array.isArray(props.additionalModels[modelName]))) {
                     return true
                 }
