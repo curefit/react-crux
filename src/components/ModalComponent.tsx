7192aa91dd0e66365c978e2bf4b17a115a0cf0c6
Yashaswini <yashaswini.hm@curefit.com>
2019-02-18 16:37:35 +0530

    Bulk upload with filter store

--- src/components/ModalComponent.tsx
+++ src/components/ModalComponent.tsx
@@ -1,7 +1,7 @@
 import autobind from "autobind-decorator"
 import * as React from "react"
 import * as _ from "lodash"
-import { Alert, ControlLabel, FormControl, FormGroup, Modal } from "react-bootstrap"
+import { Alert, Modal } from "react-bootstrap"
 import { ModalType } from "../CruxComponent"
 import { NestedEditComponent } from "./NestedEditComponent"
 
@@ -28,8 +28,7 @@ export class ModalComponent extends React.Component<ModalComponentProps, any> {
         super(props)
         this.state = {
             item: this.props.modalType === "CREATE" ? {} : this.props.item,
-            deleteModal: false,
-            syncUrl: ""
+            deleteModal: false
         }
     }
 
@@ -39,6 +38,13 @@ export class ModalComponent extends React.Component<ModalComponentProps, any> {
             console.error("Did you forget to add the representative tag at the top level.")
         }
         return repField
+    }
+
+    getInitialState() {
+        return {
+            item: this.props.modalType === "CREATE" ? {} : this.props.item,
+            deleteModal: false
+        }
     }
 
     modalPerformOperation(modalType: ModalType, edit: boolean) {
@@ -54,10 +60,6 @@ export class ModalComponent extends React.Component<ModalComponentProps, any> {
                 this.props.createOrModify(this.props.constants.modelName, this.state.item, edit, this.createOrEditSuccess, this.createOrEditError, this.props.queryParams)
             }
         }
-    }
-
-    bulkCreate = () => {
-        this.props.createOrModify(this.props.constants.modelName, this.state.syncUrl, this.createOrEditSuccess, this.createOrEditError)
     }
 
     createOrEditSuccess = (data: any) => {
@@ -80,10 +82,6 @@ export class ModalComponent extends React.Component<ModalComponentProps, any> {
     closeModal = () => {
         this.setState(Object.assign({}, this.state, _.omit(this.state, "error")))
         this.props.closeModal()
-    }
-
-    syncUrl = (e: any) => {
-            this.setState({syncUrl: e.target.value })
     }
 
     modelChanged = (value: any) => {
@@ -122,78 +120,62 @@ export class ModalComponent extends React.Component<ModalComponentProps, any> {
             dialogClassName={this.props.constants.largeEdit ? "large-modal" : ""}>
             <Modal.Header closeButton>
                 {this.props.modalType === "CREATE" &&
-                    <Modal.Title id="contained-modal-title">{"+ New " + this.props.constants.creationTitle}</Modal.Title>}
+                <Modal.Title id="contained-modal-title">{"+ New " + this.props.constants.creationTitle}</Modal.Title>}
                 {this.props.modalType === "EDIT" && <Modal.Title
                     id="contained-modal-title">{"Edit " + this.props.constants.creationTitle + " - " + this.props.item[this.getRepField().field]}</Modal.Title>}
                 {this.props.modalType === "FILTER" &&
-                    <Modal.Title id="contained-modal-title">{"Filter " + this.props.constants.creationTitle}</Modal.Title>}
+                <Modal.Title id="contained-modal-title">{"Filter " + this.props.constants.creationTitle}</Modal.Title>}
                 {this.props.modalType === "CUSTOM" &&
-                    <Modal.Title id="contained-modal-title">{"Custom " + this.props.constants.creationTitle + " - " + this.props.item[this.getRepField().field]}</Modal.Title>}
-                {this.props.modalType === "BULK_CREATE" &&
-                    <Modal.Title id="contained-modal-title">{"+ Bulk Create " + this.props.constants.creationTitle}</Modal.Title>}
+                <Modal.Title id="contained-modal-title">{"Custom " + this.props.constants.creationTitle + " - " + this.props.item[this.getRepField().field]}</Modal.Title>}
             </Modal.Header>
             <Modal.Body>
                 {this.state.error &&
-                    <Alert bsStyle="danger">
-                        {
-                            <div>
-                                {errorType && <b>{errorType}</b>}
-                                {errorMessage && <div>{errorMessage}</div>}
-                            </div>
-                        }
-                    </Alert>
+                <Alert bsStyle="danger">
+                    {
+                        <div>
+                            {errorType && <b>{errorType}</b>}
+                            {errorMessage && <div>{errorMessage}</div>}
+                        </div>
+                    }
+                </Alert>
                 }
-
-                {this.props.modalType === "BULK_CREATE" ?
-                        <FormGroup>
-                            <ControlLabel>Sync URL</ControlLabel>
-                            <FormControl type="text"
-                                         value={this.state.syncUrl}
-                                         placeholder="Sync CSV URL" onChange={this.syncUrl} />
-                        </FormGroup>
-                    : <NestedEditComponent
-                        field={this.props.constants} modalType={this.props.modalType}
-                        readonly={this.props.constants.readonly === true}
-                        additionalModels={this.props.additionalModels} fetch={this.props.fetch}
-                        modelChanged={this.modelChanged} currentModel={this.state.item}
-                        showTitle={false}
-                        parentModel={{}}
-                />}
-
+                <NestedEditComponent field={this.props.constants} modalType={this.props.modalType}
+                                     readonly={this.props.constants.readonly === true}
+                                     additionalModels={this.props.additionalModels} fetch={this.props.fetch}
+                                     modelChanged={this.modelChanged} currentModel={this.state.item}
+                                     showTitle={false}
+                                     parentModel={{}}
+                />
             </Modal.Body>
             <Modal.Footer>
                 {this.props.modalType === "EDIT" &&
-                    <div className="btn btn-danger" style={{ float: "left" }} onClick={this.openDeleteModal}>
-                        Delete</div>}
+                <div className="btn btn-danger" style={{ float: "left" }} onClick={this.openDeleteModal}>
+                    Delete</div>}
                 {this.state.deleteModal &&
-                    <Modal show={this.state.deleteModal} onHide={this.closeDeleteModal} container={this}>
-                        <Modal.Header closeButton>
-                            {"Delete " + this.props.constants.creationTitle}
-                        </Modal.Header>
-                        <Modal.Body>
-                            {"Are you sure you want to delete " + this.props.item[this.getRepField().field] + " ?"}
-                        </Modal.Body>
-                        <Modal.Footer>
-                            <div className="btn btn-danger" onClick={this.deleteModel}>Delete</div>
-                            <div className="btn btn-secondary" onClick={this.closeDeleteModal}>Cancel</div>
-                        </Modal.Footer>
-                    </Modal>
+                <Modal show={this.state.deleteModal} onHide={this.closeDeleteModal} container={this}>
+                    <Modal.Header closeButton>
+                        {"Delete " + this.props.constants.creationTitle}
+                    </Modal.Header>
+                    <Modal.Body>
+                        {"Are you sure you want to delete " + this.props.item[this.getRepField().field] + " ?"}
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <div className="btn btn-danger" onClick={this.deleteModel}>Delete</div>
+                        <div className="btn btn-secondary" onClick={this.closeDeleteModal}>Cancel</div>
+                    </Modal.Footer>
+                </Modal>
                 }
                 {this.props.modalType === "EDIT" ?
                     <>
                         <div className="btn btn-primary" onClick={this.modalPerformOperation(this.props.modalType, true)}>Update</div>
                         {this.props.constants.saveAsNew &&
-                            <div className="btn btn-primary" onClick={this.modalPerformOperation(this.props.modalType, false)}>Save as New</div>}
+                        <div className="btn btn-primary" onClick={this.modalPerformOperation(this.props.modalType, false)}>Save as New</div>}
                     </> : null}
                 {this.props.modalType === "CREATE" || this.props.modalType === "CUSTOM" ? (
                     <div className="btn btn-primary" onClick={this.modalPerformOperation(this.props.modalType, false)}>{this.props.successButtonLabel || "Create"}</div>
                 ) : null}
                 {this.props.modalType === "FILTER" ? (
                     <div className="btn btn-primary" onClick={this.modalPerformOperation(this.props.modalType, false)}>Filter</div>
-                ) : null}
-
-                {this.props.modalType === "BULK_CREATE" ? (
-                    <div className="btn btn-primary" onClick={this.bulkCreate}>Sync</div>
                 ) : null}
                 <div className="btn btn-secondary" onClick={this.closeModal}>Cancel</div>
             </Modal.Footer>
