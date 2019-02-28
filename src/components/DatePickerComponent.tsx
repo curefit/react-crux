20837ada7dd1b28aa3e8821df199937176ddd47b
Jagadeesh Y M <jagadeesh.ym@v.curefit.com>
2019-02-28 14:02:39 +0530

    initial commit

--- src/components/DatePickerComponent.tsx
+++ src/components/DatePickerComponent.tsx
@@ -1,18 +1,21 @@
 import autobind from "autobind-decorator"
 import * as React from "react"
-import * as moment from "moment"
+// import * as moment from "moment"
+import * as moment from "moment-timezone"
 import DatePicker from "react-datepicker"
 import { InlineComponentProps } from "../CruxComponent"
+const TimezonePicker = require("react-timezone")
 
 @autobind
 export class DatePickerComponent extends React.Component<InlineComponentProps, any> {
     constructor(props: any) {
         super(props)
-        this.state = { interval: 30 }
+        this.state = { interval: 30, timezone: "Asia/Kolkata" }
     }
 
     componentWillReceiveProps(nextProps: any) {
         if (nextProps.currentModel) {
+            moment.tz.setDefault(this.state.timezone)
             this.setState({ ...this.state, dateTime: moment(nextProps.currentModel) })
         }
     }
@@ -26,6 +29,7 @@ export class DatePickerComponent extends React.Component<InlineComponentProps, a
                         marginRight: "10px"
                     }}>{this.props.field.title.toUpperCase()}</label>
                     <DatePicker
+                        utcOffset={1}
                         showTimeSelect={this.props.field.showTimeSelect}
                         timeIntervals={this.state.interval}
                         dateFormat={this.props.field.showTimeSelect ? "LLL" : "LL"}
@@ -35,12 +39,29 @@ export class DatePickerComponent extends React.Component<InlineComponentProps, a
                     />
                 </div>
                 { this.props.field.showTimeSelect &&
-                <div style={{ display: "flex", flexDirection: "column" }}>
-                    <label style={{ fontSize: "10px", marginRight: "10px" }}>Time Interval</label>
+                <div style={{ display: "flex", flexDirection: "column", marginLeft: "4px" }}>
+                    <label style={{ fontSize: "10px", marginRight: "10px" }}>INTERVAL</label>
                     <input type="number" value={this.state.interval} onChange={this.handleIntervalChange} min="0" max="59" />
+                </div>}
+                { this.props.field.showTimeZone &&
+                <div style={{ display: "flex", flexDirection: "column", marginLeft: "4px" }}>
+                    <label style={{ fontSize: "10px", marginRight: "10px" }}>ZONE</label>
+                    <TimezonePicker.default
+                        value={this.state.timezone}
+                        onChange={(timezone: any) => this.handleTimezoneChange(timezone)}
+                        inputProps={{
+                            placeholder: 'Select Timezone...',
+                            name: 'timezone',
+                        }}
+                        className="font-resize"
+                    />
                 </div>}
             </div>
         )
+    }
+
+    handleTimezoneChange = (timezone: string) => {
+        this.setState({ timezone, dateTime: moment(this.state.dateTime).tz(timezone) })
     }
 
     handleIntervalChange = (event: any) => {
