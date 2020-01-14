9c3a07359c97bd9b1549143c9cf16bb5337f2dc7
Umang darji <umang.darji@v.curefit.com>
2020-01-14 14:25:38 +0530

    Time picker component Updated.

---
+++ src/components/TimePickerComponent.tsx
@@ -0,0 +1,44 @@
+import autobind from "autobind-decorator"
+import * as React from "react"
+import * as moment from "moment-timezone"
+import { InlineComponentProps } from "../CruxComponent"
+import TimePicker from 'rc-time-picker';
+import 'rc-time-picker/assets/index.css';
+
+
+const format = 'h:mm a';
+
+const now = moment()
+    .hour(0)
+    .minute(0);
+
+@autobind
+export class TimePickerComponent extends React.Component<InlineComponentProps, any> {
+    constructor(props: any) {
+        super(props)
+    }
+
+    render() {
+        const { readonly } = this.props;
+        return (
+            <div style={{ display: "flex" }}>
+                <div style={{ display: "flex", flexDirection: "column", width: "250px" }}>
+                    <label style={{
+                        fontSize: "10px",
+                        marginRight: "10px"
+                    }}>{this.props.field.title.toUpperCase()}</label>
+                    <TimePicker
+                        defaultValue={now}
+                        onChange={this.handleChange}
+                        format={format}
+                        disabled={readonly}
+                    />
+                </div>
+            </div>
+        )
+    }
+
+    handleChange = (value: any) => {
+        this.props.modelChanged(this.props.field, value)
+    }
+}
