3fc442a4b67c54d8bdb38dd9312abb268a357917
Umang darji <umang.darji@v.curefit.com>
2020-04-14 16:34:57 +0530

    default value support in select

--- src/components/NestedEditComponent.tsx
+++ src/components/NestedEditComponent.tsx
@@ -47,6 +47,9 @@ export class NestedEditComponent extends React.Component<InlineComponentProps, a
 
     getComponentForField(field: any, currentModelWithParent: any) {
         if (field.type === "select") {
+            if (field.defaultValue) {
+                this.props.currentModel && this.props.currentModel[field.field] ? null : this.props.modelChanged(Object.assign({}, this.props.currentModel, { [field.field]: field.defaultValue }))
+            }
             const currentModel = (this.props.currentModel && this.props.currentModel[field.field]) ? this.props.currentModel[field.field] : {}
             return (<SelectComponent field={field}
                 readonly={this.checkReadonly(field.readonly, currentModel)}
@@ -136,7 +139,7 @@ export class NestedEditComponent extends React.Component<InlineComponentProps, a
             )
 
 
-        } 
+        }
         else if (field.type === "timepicker") {
             const currentModel = (this.props.currentModel && this.props.currentModel[field.field]) ? this.props.currentModel[field.field] : undefined
             return (
