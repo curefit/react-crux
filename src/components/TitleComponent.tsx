51c74a0cd37eca2f506ff543fdaf6a963510290b
Umang darji <umang.darji@v.curefit.com>
2020-06-19 16:07:27 +0530

    object destrusction

--- src/components/TitleComponent.tsx
+++ src/components/TitleComponent.tsx
@@ -1,16 +1,17 @@
 import * as React from "react"
 
 export const TitleComponent = (props: any) => {
+    const { field } = props
     return (
         <label style={{
             fontSize: "10px",
             marginRight: "10px"
-        }}>{props.field.title.toUpperCase()}
-            {props.field.required ?
+        }}>{field.title.toUpperCase()}
+            {field.required ?
                 <span style={{
                     color: 'red',
                     fontSize: 11
-                }}> * </span> : null}
+                }} > * </span> : null}
         </label>
     )
 }
