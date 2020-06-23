9cd5d01a0eb9691a101ba0c31139befe9bbf1e2f
Umang darji <umang.darji@v.curefit.com>
2020-06-23 18:19:29 +0530

    Boot Strap Css

--- src/components/TitleComponent.tsx
+++ src/components/TitleComponent.tsx
@@ -3,14 +3,9 @@ import * as React from "react"
 export const TitleComponent = (props: any) => {
     const { field } = props
     return (
-        <label style={{
-            fontSize: "10px",
-            marginRight: "10px"
-        }}>{field.title.toUpperCase()}
+        <label className="small mr-2">{field.title.toUpperCase()}
             {field.required ?
-                <span style={{
-                    fontSize: 11
-                }} className="text-danger"> * </span> : null}
+                <span className="text-danger"> * </span> : null}
         </label>
     )
 }
