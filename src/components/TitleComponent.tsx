b3eddf5563ff0b9bc4198a690a0cce52cf0cfba3
Umang darji <umang.darji@v.curefit.com>
2020-09-09 18:12:25 +0530

    Small Css Change

--- src/components/TitleComponent.tsx
+++ src/components/TitleComponent.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import ReactTooltip from 'react-tooltip';
 export const TitleComponent = (props: any) => {
     const { field, isValueChanged, modalType } = props
+    console.log(isValueChanged , modalType, 'isValueChanged && modalType')
     return (
         <label className="small mr-2">{field.title.toUpperCase()}
             {field.required ?
@@ -23,8 +24,8 @@ export const TitleComponent = (props: any) => {
             {isValueChanged && modalType === "EDIT" ?
                 <img src="https://icons.iconarchive.com/icons/custom-icon-design/office/256/edit-icon.png" style={{
                     height: 15,
-                    marginLeft: 5
-            }}/> : null}
+                    marginLeft: 15
+                }} /> : null}
             <ReactTooltip />
         </label>
     )
